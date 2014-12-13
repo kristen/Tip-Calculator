65fdd9c76953138d41271adf3e68b0b0d34cee1b
Kristen Sundquist <kristen@sigfig.com>
2014-12-13 10:48:19 -0800

    Initial Commit

---
+++ TipCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalc
+//
+//  Created by Kristen on 12/13/14.
+//  Copyright (c) 2014 Kristen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TipCalc/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TipCalc
+//
+//  Created by Kristen on 12/13/14.
+//  Copyright (c) 2014 Kristen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
